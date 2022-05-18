35e5b5661a75a33bc5e72acb6be4daa63e787e74
Daniil <motunov1121@yandex.ru>
2022-05-18 15:59:56 +0300

    bear

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
 import IceCream from './Animation/IceCream/IceCream'
 import MoveBear from './Animation/MoveBear/MoveBear'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   const [page, setPage] = useState(1)
+
+
+
+  useEffect(() => {
+    setPage(Number(localStorage.getItem('page')))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('page', page)
+  }, [page])
 
   return (
     <div className='App'>
@@ -13,9 +23,9 @@ function App() {
       {page === 1 && <IceCream />}
       {page === 2 && <MoveBear />}
       <div className='pagination_container'>
-        <ul class="pagination">
-          <li onClick={() => setPage(1)} className={`waves-effect ${page === 1 && 'active'}`}><a href="#!">1</a></li>
-          <li onClick={() => setPage(2)} className={`waves-effect ${page === 2 && 'active'}`}><a href="#!">2</a></li>
+        <ul className="pagination">
+          <li onClick={() => setPage(1)} className={`waves-effect ${page === 1 && 'active'}`}><a >1</a></li>
+          <li onClick={() => setPage(2)} className={`waves-effect ${page === 2 && 'active'}`}><a >2</a></li>
           {/* <li onClick={() => setPage(3)} className={`waves-effect ${page === 3 && 'active'}`}><a href="#!">3</a></li>
           <li onClick={() => setPage(4)} className={`waves-effect ${page === 4 && 'active'}`}><a href="#!">4</a></li>
           <li onClick={() => setPage(5)} className={`waves-effect ${page === 5 && 'active'}`}><a href="#!">5</a></li> */}
