7290a8a0c888502f64dbf757dea5e323fb5d3828
Daniil <motunov1121@yandex.ru>
2022-05-19 11:14:56 +0300

    bear

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
 
   useEffect(() => {
-    setPage(Number(localStorage.getItem('page')))
+    setPage(Number(localStorage.getItem('page') || 1))
   }, [])
 
   useEffect(() => {
