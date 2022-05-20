fae3c6917e2845d44dc03f45ea87fca6d3014dda
Daniil <motunov1121@yandex.ru>
2022-05-20 23:07:33 +0300

    timer

--- src/Animation/Timer/Timer.jsx
+++ src/Animation/Timer/Timer.jsx
@@ -1,29 +1,147 @@
-import React from 'react'
-import s from './Timer.module.css'
+import React from "react";
+import s from "./Timer.module.css";
 function MoveBear() {
   return (
     <div className={s.app}>
       <div className={s.container}>
-        <div className={s.timer}>
-          <div className={s.timer_line}></div>
-          <div className={s.timer_counter}>
-            <div className={s.timer_count}>
-              <span >1</span>
-              <span >2</span>
-              <span >3</span>
-              <span >4</span>
-              <span >5</span>
-              <span >6</span>
-              <span >7</span>
-              <span >8</span>
-              <span >9</span>
-              <span >10</span>
+        <div className={s.timer_second}>
+          <div className={s.timer_second_line}></div>
+          <div className={s.timer_second_counter}>
+          <div className={s.timer_minute_count}>
+              <span>00</span>
+              <span>01</span>
+              <span>02</span>
+              <span>03</span>
+              <span>04</span>
+              <span>05</span>
+              <span>06</span>
+              <span>07</span>
+              <span>08</span>
+              <span>09</span>
+              <span>10</span>
+              <span>11</span>
+              <span>12</span>
+              <span>13</span>
+              <span>14</span>
+              <span>15</span>
+              <span>16</span>
+              <span>17</span>
+              <span>18</span>
+              <span>19</span>
+              <span>20</span>
+              <span>21</span>
+              <span>22</span>
+              <span>23</span>
+              <span>24</span>
+              <span>25</span>
+              <span>26</span>
+              <span>27</span>
+              <span>28</span>
+              <span>29</span>
+              <span>30</span>
+              <span>31</span>
+              <span>32</span>
+              <span>33</span>
+              <span>34</span>
+              <span>35</span>
+              <span>36</span>
+              <span>37</span>
+              <span>38</span>
+              <span>39</span>
+              <span>40</span>
+              <span>41</span>
+              <span>42</span>
+              <span>43</span>
+              <span>44</span>
+              <span>45</span>
+              <span>46</span>
+              <span>47</span>
+              <span>48</span>
+              <span>49</span>
+              <span>50</span>
+              <span>51</span>
+              <span>52</span>
+              <span>53</span>
+              <span>54</span>
+              <span>55</span>
+              <span>56</span>
+              <span>57</span>
+              <span>58</span>
+              <span>59</span>
             </div>
+            <div className={s.timer_second_count}>
+              <span>00</span>
+              <span>01</span>
+              <span>02</span>
+              <span>03</span>
+              <span>04</span>
+              <span>05</span>
+              <span>06</span>
+              <span>07</span>
+              <span>08</span>
+              <span>09</span>
+              <span>10</span>
+              <span>11</span>
+              <span>12</span>
+              <span>13</span>
+              <span>14</span>
+              <span>15</span>
+              <span>16</span>
+              <span>17</span>
+              <span>18</span>
+              <span>19</span>
+              <span>20</span>
+              <span>21</span>
+              <span>22</span>
+              <span>23</span>
+              <span>24</span>
+              <span>25</span>
+              <span>26</span>
+              <span>27</span>
+              <span>28</span>
+              <span>29</span>
+              <span>30</span>
+              <span>31</span>
+              <span>32</span>
+              <span>33</span>
+              <span>34</span>
+              <span>35</span>
+              <span>36</span>
+              <span>37</span>
+              <span>38</span>
+              <span>39</span>
+              <span>40</span>
+              <span>41</span>
+              <span>42</span>
+              <span>43</span>
+              <span>44</span>
+              <span>45</span>
+              <span>46</span>
+              <span>47</span>
+              <span>48</span>
+              <span>49</span>
+              <span>50</span>
+              <span>51</span>
+              <span>52</span>
+              <span>53</span>
+              <span>54</span>
+              <span>55</span>
+              <span>56</span>
+              <span>57</span>
+              <span>58</span>
+              <span>59</span>
+            </div>
+          </div>
+        </div>
+        <div className={s.timer_big_radius}>
+          <div className={s.timer_big_radius_line}></div>
+          <div className={s.timer_big_radius_counter}>
+           
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MoveBear
+export default MoveBear;
